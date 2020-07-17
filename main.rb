0c9017c86417ab1609d28e6613797459c8bcdad7
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-07-17 14:11:21 +0100

    add select method

--- main.rb
+++ main.rb
@@ -19,22 +19,36 @@ module Enumerable
     
     elsif self.instance_of?(Hash)
       for i in 0..self.length-1 do
-          yield obj[i], i
+          yield keys[i], values[i] , i
       end
     end
   end
+  
+  def my_select
+    new_arr = []
+    for i in 0..self.length-1 do
+      if yield self[i] 
+      new_arr << self[i]
+    end
+  end
+  new_arr
+  end
 end
+# ar = {"name" => "Rayhan", "sch"=> "microverse"}
 
-ar = {"name" => "Rayhan", "sch"=> "microverse"}
-arr = [1, 2, 3]
 
-ar.my_each_with_index do |(key, value), index|
-  puts "#{key} is #{value} index is = #{index}"
-end
+# arr = ["umar", "rayhan", "mj"]
 
-#ar.my_each_with_index do |i, index|
-#  puts "value = #{i} , index = #{index}"
-#end
+# p arr.my_select {|i| i != "mj" }
+
+
+# ar.my_each_with_index do |key, value, index|
+#   puts "#{key} is #{value} index is = #{index}"
+# end
+
+# #ar.my_each_with_index do |i, index|
+# #  puts "value = #{i} , index = #{index}"
+# #end
 
 
 
